9893d739e6b0543ab160558b82890ae87ed0599b
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2026-09-09 11:18:58 +0300

    feat: create LoggerMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { JobApplicationsModule } from './modules/job-applications/job-applications.module.js';
 import { PrismaModule } from './prisma/prisma.module.js';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware.js';
 
 @Module({
   imports: [PrismaModule, JobApplicationsModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
